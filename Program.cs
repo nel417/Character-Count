9539591aeeefb01aa586ff323b0a586604da6b55
Nick Landreville <45861790+nel417@users.noreply.github.com>
2019-09-11 17:01:34 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace wordcount
+{
+	class Program
+	{
+		static void Main(string[] args)
+
+		{
+			Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+			Console.WriteLine("Welcome to Character count. please enter your name to continue");
+			string name = Console.ReadLine();
+
+
+			Console.WriteLine("Hello {0}, Please paste the text from your document for a character count", name);
+			Console.ResetColor();
+
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			string input = Console.ReadLine();
+
+
+			Console.WriteLine("your character count is {0}", input.Length);
+			Console.ResetColor();
+
+			Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+			Console.WriteLine("Thank you for using Character Count, {0}, Hope to see you soon!", name);
+
+		}
+
+	}
+}
